4f3bf089085cc0a6a0a075825580a4cc35e2b790
Hardik Amal <hardik_amal2007@yahoo.com>
2016-12-20 14:39:53 +0530

    Updated opencv to v3.1 also new demo app with marshmallow support

--- ScanDemoExample/app/src/main/java/com/scanner/demo/MainActivity.java
+++ ScanDemoExample/app/src/main/java/com/scanner/demo/MainActivity.java
@@ -1,134 +1,188 @@
 package com.scanner.demo;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.View;
-import android.view.View.OnClickListener;
+import android.support.annotation.NonNull;
+import android.support.annotation.StringRes;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.scanlibrary.ScanActivity;
-import com.scanlibrary.Utils;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnNeverAskAgain;
+import permissions.dispatcher.OnPermissionDenied;
+import permissions.dispatcher.OnShowRationale;
+import permissions.dispatcher.PermissionRequest;
+import permissions.dispatcher.RuntimePermissions;
 
-public class MainActivity extends ActionBarActivity implements OnClickListener {
-
-    // ===========================================================
-    // Constants
-    // ===========================================================
+@RuntimePermissions
+public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_CODE_SCAN = 47;
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+    @Bind(R.id.imageView)
+    ImageView imageView;
+    @Bind(R.id.content_main)
+    LinearLayout contentMain;
+    @Bind(R.id.fab)
+    FloatingActionButton fab;
 
-    private static final String SAVED_SCANNED_HHOTO = "scanned_photo";
+    private static String WRITE_PERMISSIONS[] = new String[]{
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+    };
 
-    // ===========================================================
-    // Fields
-    // ===========================================================
-
-    private final ViewHolder viewHolder = new ViewHolder();
-
-    private String scannedPhoto;
-
-    // ===========================================================
-    // Constructors
-    // ===========================================================
-
-    // ===========================================================
-    // Getters & Setters
-    // ===========================================================
-
-    // ===========================================================
-    // Methods for/from SuperClass/Interfaces
-    // ===========================================================
+    private final static int REQUEST_WRITEPERMISSION_CODE = 125;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        viewHolder.prepare(findViewById(android.R.id.content));
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
 
-        if (savedInstanceState != null) {
-            scannedPhoto = savedInstanceState.getString(SAVED_SCANNED_HHOTO);
-        }
-
-        if (scannedPhoto != null) {
-            viewHolder.image.setImageBitmap(Utils.getBitmapFromLocation(scannedPhoto));
-        }
     }
 
     @Override
-    public void onClick(View v) {
-        if (v.equals(viewHolder.scabBtn)) {
-            onScanButtonClicked();
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
         }
+
+        return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        viewHolder.scabBtn.setOnClickListener(this);
-    }
+    /*@Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        viewHolder.scabBtn.setOnClickListener(null);
-    }
+        if (requestCode == REQUEST_WRITEPERMISSION_CODE) {
+
+            if (PermissionUtils.verifyAllPermissions(grantResults)) {
+                //Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+                selectImage_dialog(this);
+            } else {
+                Toast.makeText(this, "Need Permission to add this type of message", Toast.LENGTH_SHORT).show();
+
+            }
+
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+    }*/
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_CODE_SCAN && resultCode == Activity.RESULT_OK) {
             String imgPath = data.getStringExtra(ScanActivity.RESULT_IMAGE_PATH);
-            Bitmap bitmap = Utils.getBitmapFromLocation(imgPath);
-            viewHolder.image.setImageBitmap(bitmap);
-//            Uri uri = data.getExtras().getParcelable(ScanConstants.SCANNED_RESULT);
-//            Bitmap bitmap = null;
-//            try {
-//                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
-//                getContentResolver().delete(uri, null, null);
-//                viewHolder.image.setImageBitmap(bitmap);
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+            Bitmap bitmap = BitmapFactory.decodeFile(imgPath, options);
+            imageView.setImageBitmap(bitmap);
         }
     }
 
     @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putString(SAVED_SCANNED_HHOTO, scannedPhoto);
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        // NOTE: delegate the permission handling to generated method
+        MainActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
     }
 
-    // ===========================================================
-    // Methods
-    // ===========================================================
+    @NeedsPermission(Manifest.permission.CAMERA)
+    void showCamera() {
+        // NOTE: Perform action that requires the permission. If this is run by PermissionsDispatcher, the permission will have been granted
+        openScanActivity();
+    }
 
-    private void onScanButtonClicked() {
-        Intent intent = new Intent(this, ScanActivity.class);
-        intent.putExtra(ScanActivity.EXTRA_BRAND_IMG_RES, R.drawable.ic_crop_white_24dp);
-        intent.putExtra(ScanActivity.EXTRA_TITLE, "Crop Document");
-        intent.putExtra(ScanActivity.EXTRA_ACTION_BAR_COLOR, R.color.green);
-        intent.putExtra(ScanActivity.EXTRA_LANGUAGE, "en");
+
+    @OnShowRationale(Manifest.permission.CAMERA)
+    void showRationaleForCamera(PermissionRequest request) {
+        // NOTE: Show a rationale to explain why the permission is needed, e.g. with a dialog.
+        // Call proceed() or cancel() on the provided PermissionRequest to continue or abort
+        showRationaleDialog(R.string.permission_camera_rationale, request);
+    }
+
+
+    @OnPermissionDenied(Manifest.permission.CAMERA)
+    void onCameraDenied() {
+        // NOTE: Deal with a denied permission, e.g. by showing specific UI
+        // or disabling certain functionality
+        Toast.makeText(this, R.string.permission_camera_denied, Toast.LENGTH_SHORT).show();
+    }
+
+    @OnNeverAskAgain(Manifest.permission.CAMERA)
+    void onCameraNeverAskAgain() {
+        Toast.makeText(this, R.string.permission_camera_never_askagain, Toast.LENGTH_SHORT).show();
+    }
+
+
+    private void showRationaleDialog(@StringRes int messageResId, final PermissionRequest request) {
+        new AlertDialog.Builder(this)
+                .setPositiveButton(R.string.button_allow, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(@NonNull DialogInterface dialog, int which) {
+                        request.proceed();
+                    }
+                })
+                .setNegativeButton(R.string.button_deny, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(@NonNull DialogInterface dialog, int which) {
+                        request.cancel();
+                    }
+                })
+                .setCancelable(false)
+                .setMessage(messageResId)
+                .show();
+    }
+
+    @OnClick(R.id.fab)
+    public void onClick() {
+
+        MainActivityPermissionsDispatcher.showCameraWithCheck(this);
+
+
+    }
+
+
+    void openScanActivity() {
+        Intent intent = new Intent(MainActivity.this, ScanActivity.class);
+        intent.putExtra(ScanActivity.EXTRA_BRAND_IMG_RES, R.drawable.ic_crop_white_24dp); // Set image for title icon - optional
+        intent.putExtra(ScanActivity.EXTRA_TITLE, "Crop Document"); // Set title in action Bar - optional
+        intent.putExtra(ScanActivity.EXTRA_ACTION_BAR_COLOR, R.color.colorPrimary); // Set title color - optional
+        intent.putExtra(ScanActivity.EXTRA_LANGUAGE, "en"); // Set language - optional
         startActivityForResult(intent, REQUEST_CODE_SCAN);
     }
-
-
-    // ===========================================================
-    // Inner and Anonymous Classes
-    // ===========================================================
-
-    private static class ViewHolder {
-
-        ImageView image;
-        View scabBtn;
-
-        void prepare(View parent) {
-            image = (ImageView) parent.findViewById(R.id.image);
-            scabBtn = parent.findViewById(R.id.scan);
-        }
-    }
-
 }
